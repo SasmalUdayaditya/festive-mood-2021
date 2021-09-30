a70a61b943cf99d1f7288a9fdf8f5373b8915d7a
Samrat003 <souvik.samrat.003@gmail.com>
2021-10-01 00:17:44 +0530

    seervice added

---
+++ hack-demo/src/main/java/com/sp/hack/services/StudentService.java
@@ -0,0 +1,12 @@
+package com.sp.hack.services;
+
+import com.sp.hack.models.Student;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    public Student getAllStudent(){
+        return null;
+    }
+}
